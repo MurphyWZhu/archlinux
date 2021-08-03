ab24181eb1fa21bfdecae7954ae3e6aead834228
Murphy.W.Zhu <Murphy.W.Zhu@aliyun.com>
2021-08-03 13:33:17 +0800

    将dialog替换为whiptail

--- after.sh
+++ after.sh
@@ -1,42 +1,42 @@
 #! /bin/bash
 
 funerror(){
-    dialog --title $1 --textbox errorfile 20 60
+    whiptail --title $1 --textbox errorfile 20 60
     exit $2
 }
 installKde(){
-    dialog --title "Installing" --infobox "Installing kde, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing kde, please wait" 12 35
     pacman -S plasma dolphin konsole --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     systemctl enable sddm &> /dev/null
 }
 installXfce(){
-    dialog --title "Installing" --infobox "Installing xfce4, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing xfce4, please wait" 12 35
     pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings network-manager-applet pavucontrol pulseaudio --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     systemctl enable lightdm &> /dev/null
 }
 installGnome(){
-    dialog --title "Installing" --infobox "Installing gnome, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing gnome, please wait" 12 35
     pacman -S gnome --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     systemctl enable gdm &> /dev/null
 }
 installDeepin(){
-    dialog --title "Installing" --infobox "Installing Deepin, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing Deepin, please wait" 12 35
     pacman -S deepin deepin-extra --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     systemctl enable lightdm &> /dev/null
     sed -i "s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-deepin-greeter/g" /etc/lightdm/lightdm.conf
 }
 setfont /usr/share/kbd/consolefonts/iso01-12x22.psfu.gz
-ping -c 4 blog.jinjiang.fun 1> /dev/null 2> ./errorfile || funerror "NetworkError!" 1
-ADMIN_USER=$(dialog --output-fd 1 --title "ADD_User" --no-cancel --inputbox "User name:" 12 35)
+ping -c 4 www.baidu.com 1> /dev/null 2> ./errorfile || funerror "NetworkError!" 1
+ADMIN_USER=$(whiptail --output-fd 1 --title "ADD_User" --nocancel --inputbox "User name:" 12 35)
 useradd -m -G wheel ${ADMIN_USER}
 
-ADMIN_USER_PASSWD=$(dialog --output-fd 1 --title "ADD_User" --no-cancel --inputbox "User password:" 12 35)
+ADMIN_USER_PASSWD=$(whiptail --output-fd 1 --title "ADD_User" --nocancel --inputbox "User password:" 12 35)
 chpasswd <<EOF
 ${ADMIN_USER}:${ADMIN_USER_PASSWD}
 EOF
 sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" /etc/sudoers
 
-dialog --title "Iptables_config" --yesno "Enable Iptables?" 12 35 && ENABLE_IPTABLES="true" || ENABLE_IPTABLES="false"
+whiptail --title "Iptables_config" --yesno "Enable Iptables?" 12 35 && ENABLE_IPTABLES="true" || ENABLE_IPTABLES="false"
 if [ ${ENABLE_IPTABLES} = "true" ]
 then
     cp ./configfile/iptables.rules /etc/iptables/iptables.rules
@@ -44,10 +44,10 @@ then
     systemctl enable iptables ip6tables &> /dev/null
 fi
 
-DESKTOP_ENV=$(dialog --output-fd 1 --title "Select_Desktop" --menu "Select a Desktop" 12 35 5 1 no-desktop 2 XFCE 3 KDE 4 GNOME 5 Deepin)
+DESKTOP_ENV=$(whiptail --output-fd 1 --title "Select_Desktop" --menu "Select a Desktop" 12 35 5 1 no-desktop 2 XFCE 3 KDE 4 GNOME 5 Deepin)
 if [ ${DESKTOP_ENV} != "1" ]
 then
-    dialog --title "Installing" --infobox "Installing GPU drive, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing GPU drive, please wait" 12 35
     NVIDIA=0
     INTEL=0
     tmp1=$(lspci | grep -i vga | grep -i nvidia >> /dev/null)
@@ -66,9 +66,9 @@ then
     then
         pacman -S nvidia-prime --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     fi
-    dialog --title "Installing" --infobox "Installing xorg, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing xorg, please wait" 12 35
     pacman -S xorg --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
-    dialog --title "Installing" --infobox "Installing Chinese fonts, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing Chinese fonts, please wait" 12 35
     pacman -S wqy-bitmapfont wqy-microhei wqy-zenhei --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     echo "LANG=zh_CN.UTF-8
 LC_COLLATE=C" > /etc/locale.conf
@@ -85,10 +85,10 @@ LC_COLLATE=C" > /etc/locale.conf
     esac
 fi
 
-dialog --title "ARCHLINUXCN_config" --yesno "Enable archlinuxcn?" 12 35 && ARCHLINUXCN="true" || ARCHLINUXCN="false"
+whiptail --title "ARCHLINUXCN_config" --yesno "Enable archlinuxcn?" 12 35 && ARCHLINUXCN="true" || ARCHLINUXCN="false"
 if [ ${ARCHLINUXCN} = "true" ]
 then
-    dialog --title "Configuring" --infobox "Configuring archlinuxcn, please wait" 12 35
+    whiptail --title "Configuring" --infobox "Configuring archlinuxcn, please wait" 12 35
     pacman -S haveged --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     systemctl enable haveged &> /dev/null
     rm -rf /etc/pacman.d/gnupg &> /dev/null
@@ -103,4 +103,4 @@ EOF
     pacman -Syu &> /dev/null
     pacman -S archlinuxcn-keyring --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
 fi
-dialog --title "Thanks" --yesno "Install Archlinux Successful\nThanks for using this script\nMy blog: https://blog.jinjiang.fun\nReboot now?" 15 40 && reboot || exit 0
+whiptail --title "Thanks" --yesno "Install Archlinux Successful\nThanks for using this script\nMy blog: https://blog.jinjiang.fun\nReboot now?" 15 40 && reboot || exit 0
--- install.sh
+++ install.sh
@@ -1,24 +1,20 @@
 #! /bin/bash
 
 funerror(){
-    dialog --title $1 --textbox errorfile 20 60
+    whiptail --title $1 --textbox errorfile 20 60
     exit $2
 }
-installdialog(){
-    pacman -Sy &> /dev/null
-    pacman -S dialog --noconfirm &> /dev/null
-}
+
 cp ./configfile/mirrorslist /etc/pacman.d/mirrorlist
-dialog --help &> /dev/null || installdialog
 setfont /usr/share/kbd/consolefonts/iso01-12x22.psfu.gz
-ping -c 4 blog.jinjiang.fun 1> /dev/null 2> ./errorfile || funerror "NetworkError!" 1
+ping -c 4 www.baidu.com 1> /dev/null 2> ./errorfile || funerror "NetworkError!" 1
 
 timedatectl set-ntp true &> /dev/null
 
 ls /sys/firmware/efi/efivars &> /dev/null && boot_mode="uefi" || boot_mode="bios"
-DISK_NUM=$(dialog --output-fd 1 --title "Select a Disk" --menu "Select a Disk" 12 35 5 $(lsblk | grep disk | awk '{print(FNR,$1)}' | xargs))
+DISK_NUM=$(whiptail --output-fd 1 --title "Select a Disk" --menu "Select a Disk" 12 35 5 $(lsblk | grep disk | awk '{print(FNR,$1)}' | xargs))
 DISK=$(lsblk | grep disk | awk '{print($1)}' | sed -n ${DISK_NUM}p)
-dialog --title "Warning" --yesno "use this script to empty the installation disk" 12 35 || exit 0
+whiptail --title "Warning" --yesno "use this script to empty the installation disk" 12 35 || exit 0
 if [ $boot_mode = "uefi" ]
 then
     parted -s /dev/${DISK} mklabel gpt 2> ./errorfile && parted -s /dev/${DISK} mkpart ESP fat32 1M 513M 2> ./errorfile && parted -s /dev/${DISK} set 1 boot on 2> ./errorfile && parted -s /dev/${DISK} mkpart primart ext4 513M 100% 2> ./errorfile || funerror "partederror" 3
@@ -32,8 +28,8 @@ else
 fi
 
 
-dialog --title "Installing" --infobox "Installation in progress, please wait" 12 35
-pacstrap /mnt base base-devel linux linux-firmware vim networkmanager dialog 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
+whiptail --title "Installing" --infobox "Installation in progress, please wait" 12 35
+pacstrap /mnt base base-devel linux linux-firmware vim networkmanager 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
 
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
@@ -43,40 +39,43 @@ cp ./configfile/locale.gen /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen >> /dev/null
 echo 'LANG=en_US.UTF-8' >> /mnt/etc/locale.conf
 
-HOST_NAME=$(dialog --output-fd 1 --title "Hostname_Config" --no-cancel  --inputbox "Hostname:" 12 35)
+HOST_NAME=$(whiptail --output-fd 1 --title "Hostname_Config" --nocancel  --inputbox "Hostname:" 12 35)
 echo "${HOST_NAME}" >> /mnt/etc/hostname
 echo "127.0.0.1    localhost
 ::1    localhost
 127.0.1.1    ${HOST_NAME}.localdomain    ${HOST_NAME}" >> /mnt/etc/hosts
 arch-chroot /mnt systemctl enable NetworkManager &> /dev/null 
 
-ROOT_PASSWD=$(dialog --output-fd 1 --title "Password_Config" --no-cancel --inputbox "Root password:" 12 35)
+ROOT_PASSWD=$(whiptail --output-fd 1 --title "Password_Config" --nocancel --inputbox "Root password:" 12 35)
 arch-chroot /mnt chpasswd <<EOF
 root:${ROOT_PASSWD}
 EOF
 tmp1=$(cat /proc/cpuinfo | grep name | grep Intel >> /dev/null)
 if [ $? -eq 0 ]
 then
-    dialog --title "Installing" --infobox "Installing intel-ucode, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing intel-ucode, please wait" 12 35
     arch-chroot /mnt pacman -S intel-ucode --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
+else
+    whiptail --title "Installing" --infobox "Installing amd-ucode, please wait" 12 35
+    arch-chroot /mnt pacman -S amd-ucode --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
 fi
 
 
 if [ $boot_mode = "uefi" ]
 then    
-    dialog --title "Installing" --infobox "Installing and Configuring GRUB, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing and Configuring GRUB, please wait" 12 35
     arch-chroot /mnt pacman -S grub efibootmgr --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 1> /dev/null 2> ./errorfile || funerror "grub-installerror" 8
     arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg 1> /dev/null 2> ./errorfile || funerror "grub-mkconfigerror" 9
 else
-    dialog --title "Installing" --infobox "Installing and Configuring GRUB, please wait" 12 35
+    whiptail --title "Installing" --infobox "Installing and Configuring GRUB, please wait" 12 35
     arch-chroot /mnt pacman -S grub --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     arch-chroot /mnt grub-install --target=i386-pc /dev/${DISK} 1> /dev/null 2> ./errorfile || funerror "grub-installerror" 8
     arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg 1> /dev/null 2> errorfile || funerror "grub-mkconfigerror" 9
 fi
 mkdir /mnt/root/install
 cp -r ./* /mnt/root/install/
-dialog --title "Reboot" --yesno "Install Archlinux Successful\nIf you want to do the following\nPlease Reboot and Run:\n    cd /root/install\n    chmod +x after.sh\n    ./after.sh\nReboot now?" 15 40 && reboot || exit 0
+whiptail --title "Reboot" --yesno "Install Archlinux Successful\nIf you want to do the following\nPlease Reboot and Run:\n    cd /root/install\n    chmod +x after.sh\n    ./after.sh\nReboot now?" 15 40 && reboot || exit 0
 if [ ${boot_mode} = "uefi" ]
 then
     umount /mnt/boot
